59839540b85f6e40d430e401e46a2cdecc48476c
Mehedi Hasan Antor <49835373+antorinfo@users.noreply.github.com>
2020-10-28 11:45:56 +0600

    Update array.js

--- js/array.js
+++ js/array.js
@@ -32,71 +32,60 @@ let numbers = {
     two: 2
 }
 
-let key = Object.keys(numbers); // ['one','two']
-let value = Object.values(numbers); // [1,2]
-let entry = Object.entries(numbers) // [['one': 1],['two': 2]]
-
 // populating an array
 let filledArray = new Array(arraySize).fill(null),map(()=>({'hello':'goodbye'}))
 
 
-// ARRAY PROPERTIES
-//....
+// ********************* PROPERTIES ****************************
+// ************************************************************
 
-// METHODS ******************************
-// Loops Alternative 
+// ********************* METHODS ******************************
+// ************************************************************
 
-// EVERY
+////////////// EVERY
 // to check whether every element in an array meets a condition
-
 const numbers = [1,30,39,29,10,13]
 const isBelowThreshold = (num) => num < 40;
 console.log(numbers.every(isBelowThreshold));
 // true
 
-// forEach
+////////////// forEach
 // Loop through all elements and perform an action
-
 let fruits = ['apple','mango','cherry']
 fruits.forEach(fruit=> console.log(fruit))
 // apple mango cherry
 
-// FILTER
+//////////////  FILTER
 // filtering an array and get a new filtered array
 let numbers = [1,2,3,4,5,6,7]
 let odd = numbers.filter(n=> n%2===1)
 // [1,3,5,7]
 
-// MAP
+//////////////  MAP
 // to loop through all elements and get a new modified array
-
 let fruits = ['apple','mango','cherry']
 let upperCaseNames = fruits.map(fruit=> fruit.toUpperCase())
-
 //['APPLE','MANGO','CHERRY']
 
-// REDUCE
+//////////////  REDUCE
 // creating an output with array elements
 let numbers = [1,2,3,4,5]
 let result = numbers.reduce((acc,val)=> { // old + current
     acc+val,0
 })
-
 //15
 
 
-// SOME
+//////////////  SOME
 // checking if an array contains a value
-
 let fruits = ['apple','mango','cherry']
 let isApplePresent = fruits.some(fruit=>fruit==='cherry')
 if(isApplePresent) {
     console.log('Found cherry');
 }
-
 //Found cherry
 
-//SORT
+////////////// SORT
 const people = [
     {name: 'A', age: 20},
     {name: 'B', age: 21},
