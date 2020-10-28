a92dda485c67f1d56fce7974acf65f4b2c082a66
Mehedi Hasan Antor <49835373+antorinfo@users.noreply.github.com>
2020-10-28 11:57:29 +0600

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,10 @@
-// what is node ? (in the context of dom)
-// Node: every item in the dom tree is called a node. there are two types of node 1.text node 2.element node
-// 1.text node: node that has text 2. element node: node that has element. child,parent,descendent,sibling
+/*
+What is node ? (in the context of dom)
+Node: Every item in the dom tree is called a node. There are two types of node 1.Text Node 2.Element Node
+1.Text Node: Node that has text 2.Element Node: Node that has element. Child,parent,descendent,sibling
+*/
 
-// Query / Get Elements *********************
+// ***************** Query / Get Elements *********************
 // preferred way
 const hero = document.querySelector('.hero')
 const heroes = document.querySelectorAll('.hero')
@@ -12,24 +14,24 @@ const hero = document.getElementsByClassName('hero')
 const h1 = document.getElementsByTagName('h1')
 
 
-// Create / Clone Element***************
+// ***************** Create / Clone Element ***************
 let para = document.createElement('p')
 let text = document.createTextNode('I love it')
 node.cloneNode()
 node.textContent = 'some text here'
 
-// Add node to document***************
+// ******************* Add node to document***************
 parentNode.appendChild(nodeToAdd)
 parentNode.insertBefore(nodeToAdd, childNode)
 
-// Get element details*********** 
+// ***************** Get element details *****************
 node.nextElementSibling
 node.firstElementChild
 node.lastElementChild 
 node.parentElement 
 node.children 
 
-// Modify Element **********
+// ********************** Modify Element ****************
 node.style.color = 'red'
 node.style.padding = '10px'
 node.style.fontSize = '200%'
@@ -37,7 +39,7 @@ node.style.fontSize = '200%'
 node.setAttribute('attr-name','attr-value')
 node.removeAttribute('attr-name')
 
-// Get and modify element class
+// ******************* Get and modify element class ***************
 node.classList
 node.classList.add('class-name',...) 
 node.classList.remove('class-name',...)
@@ -45,12 +47,13 @@ node.classList.toggle('class-name')
 node.classList.contains('app') // true if app present
 node.classList.replace('old','new')
 
-// Remove Node *******
+// ***************** Remove Node **************
 childNode.remove()
 parentNode.removeChild(nodeToRemove)
 
 app.replaceChild()
 
+// ************* Positioning ******************
 h1.insertAdjacentHTML('beforebegin', '<span>cool</span>')
 // beforebegin => placed before h1 as a sibling
 // afterbegin => placed inside h1 as a first child
