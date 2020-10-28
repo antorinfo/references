6f5501bdb009b77632bea31d5e63745631d3cef3
Mehedi Hasan Antor <49835373+antorinfo@users.noreply.github.com>
2020-10-28 12:00:59 +0600

    Update variable.js

--- js/variable.js
+++ js/variable.js
@@ -1,15 +1,15 @@
-//shorthand
+// shorthand
 let a,b,c = 5;
 
 // 4 ways to swap vars in js
-// 1. [a,b] = [b,a]
-// 2. temp = a
-//     a = b
-//     b = temp
-// 3. a = a + b
-//    b = a - b
-//    a = a - b
-// 4. a = a ^ b
-//    b = a ^ b
-//    a = a ^ b
+1. [a,b] = [b,a]
+2. temp = a
+    a = b
+    b = temp
+3. a = a + b
+   b = a - b
+   a = a - b
+4. a = a ^ b
+   b = a ^ b
+   a = a ^ b
 
